242c21b00430378853a0429f7c50152606b2f748
Demos Petsas <demos@petsas.consulting>
2023-05-20 14:07:33 +0300

    update source files

--- api/resources/user.py
+++ api/resources/user.py
@@ -3,7 +3,7 @@ from flask_restful import Resource
 from sqlalchemy.orm import joinedload
 
 from api.schemas.user import UserSchema
-from filters.user import RoleFilter
+from filters.user import NameFilter, RoleFilter
 from models.user import User
 from sorts.user import LanguageSort
 
@@ -13,20 +13,21 @@ class UserList(Resource):
         users = (
             QueryBuilder(User)
             .allowed_filters([
-                AllowedFilter.exact("name", "first_name"),
-                AllowedFilter.partial("email"),
+                "first_name",
+                AllowedFilter.exact("surname", "last_name"),
+                AllowedFilter.custom("name", NameFilter()),
                 AllowedFilter.custom("role", RoleFilter()),
             ])
             .allowed_sorts([
                 AllowedSort.field("name", "first_name"),
-                AllowedSort.custom("language", LanguageSort())
+                AllowedSort.custom("language", LanguageSort()),
             ])
             .query
             .join(User.roles)
             .join(User.preference)
             .options(
-                joinedload(User.preference),
                 joinedload(User.roles),
+                joinedload(User.preference),
             )
             .all()
         )
--- filters/user.py
+++ filters/user.py
@@ -1,11 +1,20 @@
 from flask_query_builder.filters import Filter
 
-from models.user import Role
+from models.user import User, Role
+
+
+class NameFilter(Filter):
+    def filter(self, query, model, filter_name, values):
+        if not len(values):
+            return query
+        value = values[0]
+        return query.filter(
+            User.first_name.ilike(f"%{value}%") | User.last_name.ilike(f"%{value}%")
+        )
 
 
 class RoleFilter(Filter):
     def filter(self, query, model, filter_name, values):
-        print(values)
         if not len(values):
             return query
         return query.filter(Role.slug.in_(values))
